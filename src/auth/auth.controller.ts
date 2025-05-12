f1924341eeab41ed9d1a208e1e9d7492d97d5f46
Henrique Marques <henrique.marquesvj@gmail.com>
2025-05-12 20:44:59 -0300

    arrumando rotas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,13 +8,12 @@ export class AuthController {
     constructor(private authService: AuthService){}
     @Post('signup')
     async signup(@Body() body: SignUpDTO){
-        console.log({body});
         await this.authService.signup(body);
         return body
     }
     @Post('signin')
     async signin(@Body() body: SignInDTO){
-        console.log({body})
+        await this.authService.signin(body);
         return body
     }
 }
