8383c695a372c37b697f7ca7f9e01fb0fdc89e96
Henrique Marques <henrique.marquesvj@gmail.com>
2025-05-12 19:49:59 -0300

    acrescentando um return apenas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,9 +7,11 @@ export class AuthController {
     @Post('signup')
     async signup(@Body() body: SignUpDTO){
         console.log({body});
+        return body
     }
     @Post('signin')
     async signin(@Body() body: SignInDTO){
         console.log({body})
+        return body
     }
 }
