0f02b81b4fd870c16fb154af858d4c11a36ca30c
Henrique Marques <henrique.marquesvj@gmail.com>
2025-05-12 20:02:08 -0300

    estruturando o auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
+//Onde vamos criar a regra de negócio
 import { Injectable } from '@nestjs/common';
+import { SignInDTO, SignUpDTO } from './dtos/auth';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async signup(data: SignUpDTO){
+        console.log({data})
+        return 'signup';
+    }
+    async signin(data: SignInDTO){
+        console.log({data})
+        return 'signin';
+    }
+}
