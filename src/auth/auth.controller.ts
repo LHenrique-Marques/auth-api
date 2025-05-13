184c29d4661b704b2b6f58be500b500352aa250d
Henrique Marques <henrique.marquesvj@gmail.com>
2025-05-12 21:34:15 -0300

    arrumando

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
-// Onde vamos definir os End-Points da API
 import { Body, Controller, Post } from '@nestjs/common';
-import { SignUpDTO,SignInDTO } from './dtos/auth';
+import { SignInDTO,SignUpDTO } from './dtos/auth';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
+
     @Post('signup')
     async signup(@Body() body: SignUpDTO){
         await this.authService.signup(body);
-        return body
+        return body;
     }
     @Post('signin')
     async signin(@Body() body: SignInDTO){
         await this.authService.signin(body);
-        return body
+        return body;
     }
 }
